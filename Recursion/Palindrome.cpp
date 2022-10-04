0360a20306d2e450bc54a44c03f6f36ce6eb685b
Jakkapat Paijit <86193685+Bourbxn@users.noreply.github.com>
2022-10-04 13:23:29 +0700

    Add palindrome

---
+++ Recursion/Palindrome.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(string s, int i){
+	if(i > s.size()/2){
+	  return true ;
+	}
+	return s[i] == s[s.size()-i-1] && isPalindrome(s, i+1) ;
+}
+	
+int main()
+{
+	string str = "TeneT" ;
+	if (isPalindrome(str, 0))
+	cout << "Yes";
+	else
+	cout << "No";
+	return 0;
+}
+
